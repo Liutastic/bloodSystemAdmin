f724c4ff4e933edfa35636ffb580ac37f53ea8f9
Liutastic <githubliutastic@163.com>
2021-02-04 09:54:03 +0800

    elementUI按需导入

--- src/main.js
+++ src/main.js
@@ -2,16 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
-import elementui from 'element-ui'
+// import elementui from 'element-ui'
 import request from './utils/request'
 import 'element-ui/lib/theme-chalk/index.css'
+import './plugins/elementui'
 
 Vue.config.productionTip = false
 
-Vue.use(elementui)
+// Vue.use(elementui)
 
 // 把axios挂载在vue原型上
 Vue.prototype.$request = request
+
+// elementUI部分需要挂载在Vue原型
 new Vue({
   store,
   router,
--- src/views/Login/js/operation.js
+++ src/views/Login/js/operation.js
@@ -8,29 +8,14 @@ export default {
   methods: {
     onSubmit () {
       this.$refs['loginFormRef'].validate(valid => {
-        if (valid) this.login()
+        if (valid) this.login(this.loginForm)
       })
 
     },
-    // async login () {
-    //   let res
-    //   try {
-    //     res = await user.login()
-
-    //   } catch (err) {
-    //     return
-    //   }
-    //   if (res.code === 200) {
-    //     this.$store.commit('login', res.data.data)
-    //     const { redirect } = this.$route.query
-    //     redirect ? this.$router.push(redirect) : this.$router.push('/home')
-    //   }
-    //   return
-    // },
     async login (body) {
-      const { res } = await this.$request({ url: '/api/user/login', method: 'post', data: body })
-      if (res.code === 200) {
-        this.$store.commit('login', res.data.data)
+      const { data, data: { content } } = await this.$request({ url: '/api/user/login', method: 'post', data: body })
+      if (data.code === 200) {
+        this.$store.commit('login', content)
         const { redirect } = this.$route.query
         redirect ? this.$router.push(redirect) : this.$router.push('/home')
       }
